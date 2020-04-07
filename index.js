393c367c04724a1c7cd83c12ba0c6a39cfbd01ab
artiely <1119696785@qq.com>
2020-04-07 10:14:29 +0800

    更新

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const path = require('path')
 
 module.exports = (opts, ctx) => {
   return {
+    name:'valine',
     define: {
-      COMMENT_OPTIONS: opts.options || {},
+      COMMENT_OPTIONS: opts || {},
     },
-    enhanceAppFiles: path.resolve(__dirname, 'comment.js'),
-    globalUIComponents: 'Valine'
+    enhanceAppFiles: [path.resolve(__dirname, 'comment.js')],
+    // globalUIComponents: ['Valine']
   }
 }
